252ab22bbdc2d2a8a0e23418b4beb86af042df55
Matuk <mateusmatuk@fai-mg.br>
2020-03-11 19:39:56 -0300

    primeiro trem

---
+++ src/br/fai/collection02/Main.java
@@ -0,0 +1,25 @@
+package br.fai.collection02;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		
+		Main app = new Main();
+		app.Start();
+		
+
+	}
+	
+	private Scanner scan = new Scanner(System.in);
+	
+	
+	private void Start() {
+	
+		System.out.println("lalala");
+		
+	}
+
+}
